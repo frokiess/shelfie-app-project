b7b229f34e1ee329579adadc3f293cf6000dad4e
Oleksii <alkr2004@gmail.com>
2025-11-23 15:17:42 +0100

    feat: create shelfie app

---
+++ app/index.jsx
@@ -0,0 +1,51 @@
+import { StyleSheet, Text } from 'react-native'
+import {Link} from 'expo-router'
+import React from 'react'
+
+// thiemed components
+import ThemedView from '../components/ThemedView'
+import ThemedLogo from '../components/ThemedLogo'
+import ThemedText from '../components/ThemedText'
+import Spacer from '../components/Spacer'
+
+const Home = () => {
+  return (
+    <ThemedView style={styles.container}>
+      <ThemedLogo />
+      <Spacer height={20} />
+
+      <ThemedText style={styles.title} title={true}>
+        The number 1
+      </ThemedText>
+
+      <Spacer height={20} />
+      <ThemedText>Reading List App</ThemedText>
+      <Spacer />
+
+      <Link href="/auth/login" style={styles.link}>
+        <ThemedText>Login Page</ThemedText>
+      </Link>
+      <Link href="/auth/register" style={styles.link}>
+        <ThemedText>Register Page</ThemedText>
+      </Link>
+    </ThemedView>
+  )
+}
+
+export default Home
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  title: {
+    fontWeight: 'bold',
+    fontSize: 18
+  },
+  link: {
+    marginVertical: 10,
+    borderBottomWidth: 1
+  }
+})
